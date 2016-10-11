1e6806ad9253617ab4adbad4fcbf961c2dd2aaea
Vishal Dara <vishaldara@yahoo.com>
2016-10-11 12:44:18 +0100

    update

--- my_Flask.py
+++ my_Flask.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+stream1 = 'My experience with streem 1 was good i learn alot and i made a cool website'
+
+stream2 = 'my experience with streem 2 is ok. i am struggling at the moment but i will get there.'
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/stream')
+def list_streem():
+    return render_template('index.html', stream=stream1, second=stream2)
 
 
 if __name__ == '__main__':
